09dc133ade8d2493f116bad15383f7f7214e5fb9
jakub248248 <kubko.duban@gmail.com>
2026-04-30 14:40:43 +0200

    Refactor bouncing rectangle code for improved readability and performance 30,4,26

--- Rok2026/Analyza/ArcadeGames/bouncing_rectangle.py
+++ Rok2026/Analyza/ArcadeGames/bouncing_rectangle.py
@@ -20,7 +20,18 @@ RED = (255, 0, 0)
 pygame.init()
 
 # Set the height and width of the screen
-size = [700, 500]
+x_size = 1000
+y_size = 500
+
+# Starting position of the rectangle
+rect_x = 50
+rect_y = 50
+
+# Speed and direction of rectangle
+rect_change_x =2
+rect_change_y =2
+
+size = [x_size, y_size]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Bouncing Rectangle")
@@ -31,13 +42,6 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# Starting position of the rectangle
-rect_x = 50
-rect_y = 50
-
-# Speed and direction of rectangle
-rect_change_x = 2
-rect_change_y = 2
 
 # -------- Main Program Loop -----------
 while not done:
@@ -46,16 +50,43 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
+    if abs(rect_change_x) > 20:
+        done = True
+    if abs(rect_change_y) > 20:
+        done = True
+
     # --- Logic
     # Move the rectangle starting point
     rect_x += rect_change_x
     rect_y += rect_change_y
+    naraz = False
 
     # Bounce the ball if needed
-    if rect_y > 450 or rect_y < 0:
+    okraj_x = x_size - 50
+    okraj_y = y_size - 50
+    if rect_y > okraj_y or rect_y < 0:
+        naraz = True
         rect_change_y = rect_change_y * -1
-    if rect_x > 650 or rect_x < 0:
+    if rect_x > okraj_x or rect_x < 0:
+        naraz = True  
         rect_change_x = rect_change_x * -1
+
+    if naraz:
+        # print("Naraz!")
+        if rect_change_x > 0:
+            # print("Směr: doprava")
+            rect_change_x = rect_change_x + 1
+        elif rect_change_x < 0:
+            # print("Směr: doleva")
+            rect_change_x = rect_change_x - 1
+        if rect_change_y > 0:
+            # print("Směr: dolů")
+            rect_change_y = rect_change_y + 1
+        elif rect_change_y < 0:
+            # print("Směr: nahoru")
+            rect_change_y = rect_change_y - 1
+
+    naraz = False
 
     # --- Drawing
     # Set the screen background
@@ -63,7 +94,7 @@ while not done:
 
     # Draw the rectangle
     pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
-    pygame.draw.rect(screen, RED, [rect_x + 10, rect_y + 10, 30, 30])
+    # pygame.draw.rect(screen, RED, [rect_x + 10, rect_y + 10, 30, 30])
 
     # --- Wrap-up
     # Limit to 60 frames per second
